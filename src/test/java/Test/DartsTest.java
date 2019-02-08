d77128affcae7d953291484742a616400ad1dc65
RM147 <43373305+RM147@users.noreply.github.com>
2019-02-08 11:01:37 +0000

    fixed stuff

--- src/test/java/Test/DartsTest.java
+++ src/test/java/Test/DartsTest.java
@@ -7,17 +7,6 @@ import org.junit.Test;
 import game.Checkout;
 
 public class DartsTest {
-	@Test
-	public void test() {
-
-		assertEquals("D20", Checkout.checkoutCalculator(40));
-		assertEquals("1, D19", Checkout.checkoutCalculator(39));
-		assertEquals("Bull", Checkout.checkoutCalculator(50));
-		assertEquals("No outshot", Checkout.checkoutCalculator(1760));
-		for (int i = 170; i > 0; i--) {
-			System.out.println(i +": "+Checkout.checkoutCalculator(i));
-
-		}
-	}
+	
 
 }
